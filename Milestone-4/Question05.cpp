1ef68956de43cd946d0ea13632d570db6e994e21
BhargaviPant <91959371+BhargaviPant@users.noreply.github.com>
2021-11-30 22:54:25 +0530

    Update Question05.cpp

--- Milestone-4/Question05.cpp
+++ Milestone-4/Question05.cpp
@@ -11,7 +11,7 @@ int main()
 	int large=0, secondLarge=0;
 
 	cout<<"Enter the number of element in array: ";
-	cin>>size;//taking input from user for no of elements in array
+	cin>>size;
   	cout<<"Enter array elements: ";
 	for(i=0; i<size; i++){
 		cin>>arr[i];
